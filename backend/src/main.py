5793a3a2dc3896f0442cfaf674e1862f01dfe272
Roger Brasco <rbrasco@mlean.com>
2023-10-18 10:33:33 +0200

    Initial commit

---
+++ backend/src/main.py
@@ -0,0 +1,104 @@
+from fastapi import FastAPI, UploadFile, Query, HTTPException
+from fastapi.responses import Response
+from PIL import Image
+from io import BytesIO
+import rasterio
+import numpy as np
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# Pydantic model for image attributes
+class ImageAttributes(BaseModel):
+    width: int
+    height: int
+    bands: int
+    coordinate_reference_system: str
+    bounding_box: tuple
+
+
+# Function to open and extract image attributes
+def open_image(file):
+    try:
+        # Open the image file with rasterio
+        with rasterio.open(file) as dataset:
+            # Close SpooledTemporaryFile to avoid `Bad file descriptor` error
+            file.close()
+
+            return ImageAttributes(
+                width=dataset.width,
+                height=dataset.height,
+                bands=dataset.count,
+                coordinate_reference_system=dataset.crs.to_string(),
+                bounding_box=dataset.bounds,
+            )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# Function to create a thumbnail image
+def create_thumbnail(file, resolution=100):
+    try:
+        # Open the image file with rasterio
+        with rasterio.open(file) as dataset:
+            # Extract the bands for RGB composition
+            data = dataset.read([4, 3, 2])
+            # Close SpooledTemporaryFile to avoid `Bad file descriptor` error
+            file.close()
+
+            # Normalize the data with the Typical Range and convert to 8-bit unsigned integers
+            data = (data * (255 / 4000)).astype(np.uint8)
+            # Merge the three matrices to a matrix with triplets with values (R,B,G)
+            data = np.rec.fromarrays([data[0], data[1], data[2]])
+
+            # Create a Pillow image
+            image = Image.fromarray(data, mode="RGB")
+
+            # Create a thumbnail
+            image.thumbnail((resolution, resolution))
+
+            # Save the thumbnail to a BytesIO object
+            thumbnail_io = BytesIO()
+            image.save(thumbnail_io, format="PNG")
+
+        # Return the thumbnail as a response
+        return Response(content=thumbnail_io.getvalue(),
+                        media_type="image/png")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# Test route
+@app.get(
+    "/",
+    summary="Root Endpoint",
+    description="A simple root endpoint for testing the FastAPI application.")
+async def root():
+    return {"message": "Hello from EarthPulse BE!"}
+
+
+# Endpoint to get image attributes
+@app.post(
+    "/attributes",
+    summary="Get Image Attributes",
+    description=
+    "Upload an image file and retrieve its attributes such as width, height, bands, CRS, and bounding box."
+)
+async def get_image_attributes(image_file: UploadFile):
+    return open_image(image_file.file)
+
+
+# Endpoint to get a thumbnail
+@app.post(
+    "/thumbnail",
+    summary="Get Thumbnail",
+    description=
+    "Upload an image file and generate a thumbnail with an optional resolution setting."
+)
+async def get_thumbnail(
+    image_file: UploadFile,
+    resolution: int = Query(100,
+                            description="Thumbnail resolution (pixels).")):
+    return create_thumbnail(image_file.file, resolution)
---
+++ backend/tests/tests.py
@@ -0,0 +1,36 @@
+from PIL import Image
+from io import BytesIO
+from src.main import open_image, create_thumbnail
+from tempfile import SpooledTemporaryFile
+
+test_image = "./tests/S2L2A_2022-06-09.tiff"
+
+# Create a BytesIO object for testing
+def create_test_image():
+    with open(test_image, "rb") as f:
+        spooled = SpooledTemporaryFile(max_size=1e9)
+        spooled.write(f.read())
+    spooled.seek(0)
+    return BytesIO(spooled.read())
+
+# Test case for the get_image_attributes endpoint
+def test_get_image_attributes():
+    test_image = create_test_image()
+    response = open_image(test_image)
+    data = response.model_dump_json()
+    assert "width" in data
+    assert "height" in data
+    assert "bands" in data
+    assert "coordinate_reference_system" in data
+    assert "bounding_box" in data
+
+# Test case for the get_thumbnail endpoint
+def test_get_thumbnail():
+    test_image = create_test_image()
+    resolution = 50
+    response = create_thumbnail(test_image, resolution)
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "image/png"
+    thumbnail_data = response.body
+    img = Image.open(BytesIO(thumbnail_data))
+    assert resolution in img.size 
